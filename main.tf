5b402587f5cdc597d74b737911b20b5362aa1c96
Dev Ajaiji <devajaiji@DevAjaiji.local>
2024-09-12 11:01:08 +0400

    add

--- main.tf
+++ main.tf
@@ -1,31 +1,43 @@
-###### root/main.tf
-/*
-module "eks" {
-  source                  = "./modules/eks"
-  aws_public_subnet       = module.vpc.aws_public_subnet
-  vpc_id                  = module.vpc.vpc_id
-  cluster_name            = "module-eks-${random_string.suffix.result}"
-  endpoint_public_access  = true
-  endpoint_private_access = false
-  public_access_cidrs     = ["0.0.0.0/0"]
-  node_group_name         = "cloudquicklabs"
-  scaling_desired_size    = 1
-  scaling_max_size        = 1
-  scaling_min_size        = 1
-  instance_types          = ["t3.large"]
-  key_pair                = "TestKeyPair"
+terraform {
+  backend "s3" {
+    bucket         = "s306022024"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+  }
 }
 
-module "vpc" {
-  source                  = "./modules/vpc"
-  tags                    = "cloudquicklabs"
-  instance_tenancy        = "default"
-  vpc_cidr                = "10.0.0.0/16"
-  access_ip               = "0.0.0.0/0"
-  public_sn_count         = 2
-  public_cidrs            = ["10.0.1.0/24", "10.0.2.0/24"]
-  map_public_ip_on_launch = true
-  rt_route_cidr_block     = "0.0.0.0/0"
-
+provider "aws" {
+  region = var.aws_region
 }
-*/
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  tags = {
+    Name = var.name
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum -y update
+              sudo yum -y install httpd
+              sudo systemctl start httpd
+              sudo systemctl enable httpd
+              EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
